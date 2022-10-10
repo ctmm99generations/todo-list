7eb5c3157fd49df83624b7340d236eca23744fa1
TakahiroT0415156 <teradatakahiro16@gmail.com>
2022-10-10 15:01:45 +0900

    add tailwind components folder

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+function TodoList() {
+  return (
+    <>
+      <ul>
+        <li></li>
+        <Button />
+        <Button />
+      </ul>
+    </>
+  );
+}
+
+export default TodoList;
