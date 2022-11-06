f6c18fddb5d268b38606561147405ea7989a10c0
TakahiroT0415156 <teradatakahiro16@gmail.com>
2022-11-06 20:16:41 +0900

    firebaseへの連携完了

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,4 @@
 function Button(props) {
-  console.log(props);
   const { name, clickFunc } = props
 
   return <button onClick={clickFunc}>{name}</button>;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { collection, addDoc, doc, getDocs, deleteDoc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase.js";
 import Button from "./Button";
 import Form from "./Form";
@@ -11,45 +11,48 @@ function TodoList() {
   const [inputTodo, setInputTodo] = useState('');
   const changeInputTodo = (e) => setInputTodo(e.target.value)
 
+  useEffect(() => {
+    const todoCollectionRef = collection(db, 'todos');
+    getDocs(todoCollectionRef).then((querySnapshot) => {
+      setTodos(
+        querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    });
+  }, []);
+
   const postBtn = () => {
     if(!inputTodo) return;
-    const todoText = {todo: inputTodo, active: false}
-    const newTodos = [...todos, todoText]
     addDoc(collection(db, "todos"), {
       todo: inputTodo,
       active: false,
     })
-    setTodos(newTodos)
     setInputTodo('')
   }
 
-  const completeBtn = (index) => {
-    const newTodos = [...todos]
-    if (newTodos[index].active === false) {
-      newTodos[index].active = true
-    } else {
-      newTodos[index].active = false
-    }
-    setTodos(newTodos)
-  }
+  const completeTodo = async (id) => {
+    const todoDocumentRef = doc(db, 'todos', id);
+    await updateDoc(todoDocumentRef, {
+      active: true,
+    });
+  };
 
-  const deleteBtn = (index) => {
-    const newTodos = [...todos]
-    newTodos.splice(index, 1)
-    setTodos(newTodos)
-  }
+  const deleteTodo = async (id) => {
+    const todoDocumentRef = doc(db, 'todos', id);
+    console.log(todoDocumentRef);
+    await deleteDoc(todoDocumentRef);
+  };
 
-  const todolists = todos.map((moji, index) => {
+  const todolists = todos.map((todo) => {
     return (
-      <li key={index}>
-        <span className={moji.active ? "todo" : ""}>{moji.todo}</span>
+      <li key={todo.id}>
+        <div className={todo.active ? "todo" : ""}>{todo.todo}</div>
         <Button
           name='完了'
-          clickFunc={() => completeBtn(index)}
+          clickFunc={() => completeTodo(todo.id)}
         />
         <Button
           name='削除'
-          clickFunc={() => deleteBtn(index)}
+          clickFunc={() => deleteTodo(todo.id)}
         />
       </li>
     )
